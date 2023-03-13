93a6b943f46b20ab5c22e8aa09dc0b9abe7f6f08
angelique yoanita <aangeliqueyoanita@student.uns.ac.id>
2023-03-13 18:28:31 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,34 @@
+#Tugas Praktikum Prokom 
+
+print("*"*90)
+print("\n"+"Nama : Angelique Yoanita")
+print("Kelas : A (Teknik Industri)")
+print("NIM : I0322014"+"\n")
+print("*"*90+"\n")
+
+print("""
+    Perkenalkan nama saya Angelique Yoanita, saya berasal dari Tangerang namun saya 
+lahir di Jakarta, 12 Juli 2004 dengan proses lahiran caesar. Saya adalah anak kedua 
+dari empat bersaudara dan saya adalah anak kedua yang sedang menempuh pendidikan di 
+tingkat kuliah. Saya berkuliah di Universitas Sebelas Maret jurusan Teknik Industri
+dan sekarang saya sedang menempuh semester dua. Tangerang adalah kota dimana saya 
+tumbuh dan berkembang sejak bayi hingga umur 18 tahun. 
+    Tangerang bukanlah kota yang penuh akan destinasi wisata atau tempat yang baik 
+untuk healing dari penatnya kerjaan namun Tangerang adalah safe place bagi saya, 
+dimana keluarga dan teman - teman terdekat saya tinggal. Bagi saya Tangerang adalah 
+kota yang penuh polusi dan udaranya sangat panas namun di Tangerang saya bisa 
+mendapatkan apa yang saya mau dengan mudah.Tangerang merupakan kampung halaman saya 
+tapi kalau keluarga saya ingin pulang kampung maka kami akan pulang ke medan lebih 
+tepatnya adalah Tebing Tinggi. Tebing Tinggi adalah salah satu kota yang terletak di 
+Sumatra Utara dekat dengan kota Medan dan Siantar.Tebing tinggi merupakan kota yang 
+sangat asri dan sangat menyenangkan dimana udara disana masih bagus dan lingkungan 
+yang masih kental akan budaya.
+    Sebagai salah satu mahasiswi Teknik Industri harapan saya adalah ingin menjadi 
+salah satu mahasiswi yang berprestasi dan membanggakan. Selain itu saya berharap bisa
+menjadi mahasiswi yang berguna bagi lingkungan sekitar, bangsa dan negara. Bisa
+mendapat ilmu yang baik dan berguna bagi dunia kerja nanti. Menempuh kuliah dengan 
+produktif dan mengupgrade diri kearah yang lebih baik dan bekerja sesuai dengan
+bidang yang saya minati dan sukai.""" )
+
+
+print("\n"+"\n"+"*"*90)
